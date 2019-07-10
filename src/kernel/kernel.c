25499da6a488bcdd0fa2f33f046e17bcb7894f50
Evan Pratten <ewpratten@gmail.com>
2019-07-10 11:00:23 -0400

    sdt libs

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,17 +1,24 @@
 #include <kernel/uart.h>
+#include <extras/stdio.h>
+#include <extras/stdlib.h>
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 {
+    // Input buffer
+    char buf[256];
+    
     // Declare as unused
-    (void) r0;
-    (void) r1;
-    (void) atags;
+    (void)r0;
+    (void)r1;
+    (void)atags;
 
     uart_init();
-    uart_puts("Hello, kernel World!\r\n");
+    puts("Hello, kernel World!\r\n");
 
-    while (1) {
-        uart_putc(uart_getc());
-        uart_putc('\n');
+    while (1)
+    {
+        gets(buf, 256);
+        puts(buf);
+        putc('\n');
     }
 }
